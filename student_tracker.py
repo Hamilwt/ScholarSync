6977d7651bd11ce163ddc11afe3ce5563ce0b64f
Mohammed Hamil P R <mohammedhamil100@gmail.com>
2025-07-30 00:28:32 +0530

    Update student_tracker.py

--- student_tracker.py
+++ student_tracker.py
@@ -14,7 +14,7 @@ PAGE_ICON = "🎓"
 LAYOUT = "wide"
 STUDENTS_COLLECTION = "students"
 CHAT_COLLECTION = "chat"
-ACADEMIC_PROGRESS_LEVELS = ["Excellent", "Good", "Average", "Poor"]
+ACADEMIC_PROGRESS_LEVELS = ["Excellent", "Good", "Average", "Needs Improvement"]
 
 
 # --- Firebase Initialization ---
